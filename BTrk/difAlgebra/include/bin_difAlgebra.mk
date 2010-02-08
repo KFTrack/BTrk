34c6fc72e74466139597738b68c5de1a9ad2cce4
brownd <brownd1978@gmail.com>
2010-02-08 22:20:55 +0000

    Initial copy from SuperB repo

---
+++ BTrk/difAlgebra/include/bin_difAlgebra.mk
@@ -0,0 +1,6 @@
+# Please see SoftRelTools/HOWTO-dependency for documentation
+# $Id: bin_difAlgebra.mk 501 2010-01-14 12:46:50Z stroili $
+override LINK_BaBar                += difAlgebraGNUmakefile
+override LINK_BbrGeom              += difAlgebraGNUmakefile
+override LINK_CLHEP                += difAlgebraGNUmakefile
+override LINK_m                    += difAlgebraGNUmakefile
