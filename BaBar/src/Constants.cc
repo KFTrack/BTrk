2fe965a645e39689ebf9721085a21d1d72ba84f9
kutschke <kutschke@fnal.gov>
2010-04-22 18:19:50 +0000

    Give values to the static const data members.

---
+++ BaBar/src/Constants.cc
@@ -0,0 +1,30 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id:$
+//
+// Description:
+//	The Constants class contains various constant values as
+//      static members.
+//
+// Environment:
+//      This version is a stripped down version for the port to Mu2e.
+//
+// Author List:
+//      Bob Jacobsen 
+//      Rob Kutschke, Dave Brown (port to Mu2e).
+//
+// Copyright Information:
+//	Copyright (C) 1995, 1996
+//
+//------------------------------------------------------------------------
+#include "BaBar/Constants.hh"
+#include "CLHEP/Units/PhysicalConstants.h"
+
+const double Constants::pi           = CLHEP::pi;
+const double Constants::twoPi        = CLHEP::twopi;
+const double Constants::radToDegrees = 180./Constants::pi;
+
+const double Constants::c  = CLHEP::c_light/CLHEP::centimeter;
+
+const double Constants::epsilon = 1.e-6;
+
