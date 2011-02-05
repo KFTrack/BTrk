85e2b66635b05556dda1bc608bc402c44904ffe1
brownd <brownd@0f8b7496-127b-11df-8f55-bc4be6f0abc2>
2011-02-05 15:00:42 +0000

    Fix typo

--- mu2eFast/trunk/src/Mu2eInput.cc
+++ mu2eFast/trunk/src/Mu2eInput.cc
@@ -2,7 +2,7 @@
 //  Define the input file structure for reading ROOT data particles, and setup
 // a structure to read/rewind etc.  David Brown, LBNL 7 Jan. 2011
 //
-#include "mu2eFast/mu2eInput.hh"
+#include "mu2eFast/Mu2eInput.hh"
 
 Mu2eInput::Mu2eInput(){
 }
