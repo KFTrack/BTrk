472c4a9ae6ae6195ba3cd72a151c8aaab515ff8c
brownd <brownd1978@gmail.com>
2011-03-14 19:47:54 +0000

    fix typo

--- BTrk/mu2eFast/macros/mu2e_trkreco.C
+++ BTrk/mu2eFast/macros/mu2e_trkreco.C
@@ -124,7 +124,6 @@ void mu2e_trkreco(TCanvas* can,TTree* tree, const char* cpage="rec" ) {
     gPad->SetLogy();
     fitprob->Fit("pol1","","",0.05,1);
   } else if(page == "selection")  {
-    TH1F* 
   } else if(page == "eff"){
 
     TH1F* td_s = new TH1F("td_s","TanDip",100,0.5,1.1);
