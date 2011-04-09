0ad26327d2b3baa25d5b3ed8f5e85ebd1c61271a
brownd <brownd1978@gmail.com>
2011-04-09 07:14:05 +0000

    add gradient test

--- mu2eFast/src/mu2eGradientField.cc
+++ mu2eFast/src/mu2eGradientField.cc
@@ -16,7 +16,7 @@ using namespace std;
 mu2eGradientField::mu2eGradientField(double b0,double z0, double b1, double z1, double rmax) :
 _b0(0,0,b0),_b1(0,0,b1),_z0(z0),_z1(z1),_rmax(rmax)
 {
-  assert(z0 < z1 && z1 < 0.0);
+  assert(z0 < z1);
 // compute the gradient
   _grad = (b1-b0)/(z1-z0);
 }
@@ -34,9 +34,8 @@ mu2eGradientField::bFieldVect (const HepPoint &point)const {
     double bgrad = _grad*(point.z()-_z0);
 // work in cylindrical coordinates
     double bz = _b0.z()+bgrad;
-    double rad = point.perp();
-    double bx = -_grad*point.x()/rad;
-    double by = -_grad*point.y()/rad;
+    double bx = -_grad*point.x();
+    double by = -_grad*point.y();
     return Hep3Vector(bx,by,bz);
   }
 }
