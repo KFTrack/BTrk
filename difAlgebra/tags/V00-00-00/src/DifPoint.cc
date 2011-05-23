9a54caba1f522d0cafb0dbaabd99a03d1c5a42aa
brownd <brownd@0f8b7496-127b-11df-8f55-bc4be6f0abc2>
2011-05-23 16:23:53 +0000

    "Tag pre-migration version"

---
+++ difAlgebra/tags/V00-00-00/src/DifPoint.cc
@@ -0,0 +1,23 @@
+//--------------------------------------------------------------------------
+// File and Version Information:
+// 	$Id: DifPoint.cc 501 2010-01-14 12:46:50Z stroili $
+//
+// Description:
+//	Class Implementation for |DifPoint| 
+//      What do i do ?
+// Environment:
+//	Software developed for the BaBar Detector at the SLAC B-Factory.
+//
+// Author List:
+//	A. Snyder
+//
+// Copyright Information:
+//	Copyright (C) 1996	SLAC
+//
+//------------------------------------------------------------------------
+
+#include "BaBar/BaBar.hh"
+
+#include "difAlgebra/DifPoint.hh"
+
+extern const DifPoint origin(0,0,0);
