4027e2494a47a82a7b52744432bed0f1fcfc247a
kutschke <kutschke@fnal.gov>
2011-06-03 14:09:37 +0000

    Move from V00-00-00 to trunk.

--- BTrk/DetectorModel/src/DetIntersection.cc
+++ BTrk/DetectorModel/src/DetIntersection.cc
@@ -18,47 +18,49 @@
 //  Constructors
 //
 DetIntersection::DetIntersection() :
-  delem(0),pathlen(0.0),trajet(0)
+  delem(0), trajet(0), pathlen(0.0)
 {
-  pathrange[0] = pathrange[1] = 0.0; 
+  pathrange[0] = pathrange[1] = 0.0;
   flag[0] = flag[1] = 0;
 }
 DetIntersection::DetIntersection(const DetIntersection& other) :
-  delem(other.delem),pathlen(other.pathlen),trajet(other.trajet)
+  delem(other.delem), trajet(other.trajet), pathlen(other.pathlen)
 {
   pathrange[0] = other.pathrange[0];
   pathrange[1] = other.pathrange[1];
-  flag[0]      = other.flag[0];
-  flag[1]      = other.flag[1];
+  flag[0] = other.flag[0];
+  flag[1] = other.flag[1];
 }
 
-DetIntersection::DetIntersection(const DetElem* elem,const Trajectory* traj,double len):
-  delem(elem),pathlen(len),trajet(traj)
+DetIntersection::DetIntersection(const DetElem* elem, const Trajectory* traj,
+    double len) :
+  delem(elem), trajet(traj), pathlen(len)
 {
   pathrange[0] = pathrange[1] = len;
   flag[0] = flag[1] = 0;
 }
 
-DetIntersection::DetIntersection(const DetElem* elem,const Trajectory* traj,double range[2]):
-  delem(elem),trajet(traj)
+DetIntersection::DetIntersection(const DetElem* elem, const Trajectory* traj,
+    double range[2]) :
+  delem(elem), trajet(traj)
 {
   pathlen = pathrange[0] = range[0];
   pathrange[1] = range[1];
   flag[0] = flag[1] = 0;
 }
 
-DetIntersection::DetIntersection(const DetElem* elem,const Trajectory* traj,
-					   double lowrange,double highrange):
-  delem(elem),pathlen(lowrange),trajet(traj)
+DetIntersection::DetIntersection(const DetElem* elem, const Trajectory* traj,
+    double lowrange, double highrange) :
+  delem(elem), trajet(traj), pathlen(lowrange)
 {
   pathrange[0] = lowrange;
   pathrange[1] = highrange;
   flag[0] = flag[1] = 0;
 }
 
-DetIntersection::DetIntersection(const DetElem* elem,const Trajectory* traj,double path,
-					   double lowrange,double highrange):
-  delem(elem),pathlen(path),trajet(traj)
+DetIntersection::DetIntersection(const DetElem* elem, const Trajectory* traj,
+    double path, double lowrange, double highrange) :
+  delem(elem), trajet(traj), pathlen(path)
 {
   pathrange[0] = lowrange;
   pathrange[1] = highrange;
