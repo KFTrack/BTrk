6aa82c72efa68bae23f5f9e52095c209ececea51
brownd <brownd1978@gmail.com>
2012-11-15 22:06:51 +0000

    Improve field integration implementation and interface

--- BField/src/BFieldIntegrator.cc
+++ BField/src/BFieldIntegrator.cc
@@ -15,99 +15,79 @@
 #include <vector>
 
 
-BFieldIntegrator::BFieldIntegrator(const BField &f) :
-  _field(f),_bNominal(0.0, 0.0, f.bFieldNominal()), _tolerance(0.1), _pathMin(
-      0.5), _stepFrac(0.1), _stepCeiling(5.0), _divTolerance(0.1), _divPathMin(
-      0.5), _divStepFrac(0.1), _divStepCeiling(5.0)
-{
-  //  cerr << "BFieldIntegrator: _bNominal = " << _bNominal << endl;
-  //  f.print(cerr);
-}
+BFieldIntegrator::BFieldIntegrator(const BField &f,BFieldIntConfig const& config) :
+  _field(f),_config(config),_bNominal(0.0, 0.0, f.bFieldNominal())
+{}
 
 
-BFieldIntegrator::BFieldIntegrator(const BField &f,const Hep3Vector& bnom) :
-  _field(f), _bNominal(bnom), _tolerance(0.1), _pathMin(
-      0.5), _stepFrac(0.1), _stepCeiling(5.0), _divTolerance(0.1), _divPathMin(
-      0.5), _divStepFrac(0.1), _divStepCeiling(5.0)
-{
-  //  cerr << "BFieldIntegrator: _bNominal = " << _bNominal << endl;
-  //  f.print(cerr);
-}
+BFieldIntegrator::BFieldIntegrator(const BField &f,BFieldIntConfig const& config,const Hep3Vector& bnom) :
+  _field(f),_config(config), _bNominal(bnom)
+{}
 
 BFieldIntegrator::~BFieldIntegrator()
-{
-}
+{}
 
 Hep3Vector
-BFieldIntegrator::deltaMomentum(const Trajectory *traj, double range[2]) const
+BFieldIntegrator::deltaMomentum(const Trajectory *traj, BFieldIntRange const& range) const
 {
-  double slo = range[0];
-  double sup = range[1];
-  return deltaMomentum(traj, slo, sup);
-}
-
-Hep3Vector
-BFieldIntegrator::deltaMomentum(const Trajectory *traj, double slo, double sup) const
-{
-
-  HepPoint beg = traj->position(slo);
-  HepPoint end = traj->position(sup);
-  double deltas = sup - slo;
-  double smid = (slo + sup) / 2;
-  double test = deltas
-      * (_field.bFieldVect(end) - _field.bFieldVect(beg)).mag();
-  if (fabs(test) <= _tolerance || deltas < pathMin()) {
-    HepPoint mid = traj->position(smid);
-    Hep3Vector bMid = field().bFieldVect(mid);
-    bMid -= _bNominal;
-    Hep3Vector dir = traj->direction(smid);
-    Hep3Vector temp = dir * deltas;
-    temp *= BField::mmTeslaToMeVc;
-    return temp.cross(bMid);
-  } else {
-    return deltaMomentum(traj, slo, smid) + deltaMomentum(traj, smid, sup);
-  }
-}
-
-void
-BFieldIntegrator::divideRange(const Trajectory *traj, double range[2],
-    std::vector<double>& posList) const
-{
-  double slo = range[0];
-  double sup = range[1];
-  divideRange(traj, slo, sup, posList);
-}
-
-void
-BFieldIntegrator::divideRange(const Trajectory *traj, double slo, double sup,
-    std::vector<double>& posList) const
-{
-  // trivial test first
-  double deltas = sup - slo;
-  if (deltas > divPathMin()) {
-    double smid = (slo + sup) / 2.;
-    HepPoint mid = traj->position(smid);
+  static Hep3Vector null(0.0,0.0,0.0);
+  Hep3Vector retval = null;
+  HepPoint beg = traj->position(range._slo);
+  HepPoint end = traj->position(range._shi);
+  double deltas = range._shi - range._slo;
+  if(deltas <  _config._intPathMin ) {
+    HepPoint beg=traj->position(range._slo);
+    HepPoint end=traj->position(range._shi);
+    HepPoint mid=traj->position(range._smid);
+    Hep3Vector bbeg(_field.bFieldVect(beg));
+    Hep3Vector bend(_field.bFieldVect(end));
     Hep3Vector bmid(_field.bFieldVect(mid));
-    //    HepPoint beg=traj->position(slo);
-    //    HepPoint end=traj->position(sup);
-    double test = deltas * (bmid - _bNominal).mag();
-    //    double change = (_field.bFieldVect(end)-_field.bFieldVect(beg)).mag();
-    // if the current interval is out of tolerance or too large, subdivide it
-    if (test >= _divTolerance || deltas >= divStepCeiling()) {
-      //	 change >= _divTolerance ){
-      divideRange(traj, slo, smid, posList);
-      divideRange(traj, smid, sup, posList);
-      return;
+    double db = (bend -bbeg).mag();
+    Hep3Vector bdiff = bmid - _bNominal;
+// compute the change in momentum from this
+    double dpmax = deltas * db * BField::mmTeslaToMeVc;
+    if (dpmax < _config._intTolerance){
+      Hep3Vector dir = traj->direction(range._smid) * deltas * BField::mmTeslaToMeVc;
+      retval = dir.cross(bdiff);
+    }
+  } else if(deltas < _config._maxRange) {
+    retval = deltaMomentum(traj, BFieldIntRange(range._slo, range._smid)) + deltaMomentum(traj, BFieldIntRange(range._smid, range._shi));
+  } 
+  return retval;
+}
+
+void
+BFieldIntegrator::divideRange(const Trajectory *traj,BFieldIntRange const& range,
+    std::vector<BFieldIntRange>& divrange) const
+{
+  bool divide(false);
+  double deltas = range._shi - range._slo;
+// trivial tests first
+  if (deltas > _config._divPathMin && deltas < _config._maxRange) {
+    divide = deltas > _config._divStepCeiling;
+    if(!divide){
+// calculate the fractional change in momentum over this range
+      HepPoint beg=traj->position(range._slo);
+      HepPoint end=traj->position(range._shi);
+      HepPoint mid=traj->position(range._smid);
+      Hep3Vector bbeg(_field.bFieldVect(beg));
+      Hep3Vector bend(_field.bFieldVect(end));
+      Hep3Vector bmid(_field.bFieldVect(mid));
+      Hep3Vector bavg = (bbeg+bmid+bend)/3.0;
+      // Find the largest of either the difference with the nominal, or the change over the range
+      double db = (bend -bbeg).mag();
+      double bdiff = (bavg - _bNominal).mag();
+      double deltab = std::max(db,bdiff);
+      // compute the change in momentum from this
+      double dpmax = deltas * deltab * BField::mmTeslaToMeVc;
+      divide = dpmax > _config._divTolerance;
     }
   }
-  // otherwise, append these to the list and end
-  // append is OK to use here instead of insert since the
-  // above recursion guarantees the points will be entered in
-  // monotonic order.  If the above algorithm is changed, please
-  // make sure that this assumption still holds, or convert to
-  // using insert().
-  posList.push_back(slo);
-  posList.push_back(sup);
-  return;
+  if(divide){
+    divideRange(traj, BFieldIntRange(range._slo, range._smid), divrange);
+    divideRange(traj, BFieldIntRange(range._smid, range._shi), divrange);
+  } else {
+    divrange.push_back(range);
+  }
 }
 
