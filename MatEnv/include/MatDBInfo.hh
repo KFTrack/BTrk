cf93922cba82a7b90ae5aa155b0702bafb13dd65
tassiell <tassiell@0f8b7496-127b-11df-8f55-bc4be6f0abc2>
2012-11-20 04:29:31 +0000

    some updates to be possible to switch between different Detmaterial classes

--- MatEnv/include/MatDBInfo.hh
+++ MatEnv/include/MatDBInfo.hh
@@ -21,6 +21,9 @@
 #define MATDBINFO_HH
 
 #include "MatEnv/MaterialInfo.hh"
+#include "MatEnv/RecoMatFactory.hh"
+#include "MatEnv/MtrPropObj.hh"
+#include "ErrLogger/ErrLog.hh"
 
 class DetMaterial;
 class RecoMatFactory;
@@ -37,9 +40,10 @@ public:
   virtual ~MatDBInfo();
 //  Find the material, given the name
   virtual const DetMaterial* findDetMaterial( const std::string& matName ) const;
+  template <class T> const T* findDetMaterial( const std::string& matName ) const;
 // utility functions
 private:
-  DetMaterial* createMaterial( const std::string& dbName, 
+  template <class T> T* createMaterial( const std::string& dbName,
 			       const std::string& detMatName ) const;
   void declareMaterial( const std::string& dbName, 
 			const std::string& detMatName );
@@ -57,5 +61,56 @@ private:
   friend class MatBuildEnv;
   friend class MatBuildCoreEnv;
 };
+
+template <class T> T*
+MatDBInfo::createMaterial( const std::string& db_name,
+                           const std::string& detMatName ) const
+{
+  MtrPropObj* genMtrProp;
+  T* theMat;
+
+  if (_genMatFactory == 0)
+    that()->_genMatFactory = RecoMatFactory::getInstance();
+
+  genMtrProp = _genMatFactory->GetMtrProperties(db_name);
+  if(genMtrProp != 0){
+    theMat = new T( detMatName.c_str(), genMtrProp ) ;
+    that()->_matList[new std::string( detMatName )] = theMat;
+    return theMat;
+  } else {
+    return 0;
+  }
+}
+
+template <class T> const T*
+MatDBInfo::findDetMaterial( const std::string& matName ) const
+{
+  if (_genMatFactory == 0)
+    that()->_genMatFactory = RecoMatFactory::getInstance();
+
+  T* theMat;
+  std::map< std::string*, DetMaterial*, PtrLess >::const_iterator pos;
+  if ((pos = _matList.find((std::string*)&matName)) != _matList.end()) {
+    theMat = (T*) pos->second;
+  } else {
+// first, look for aliases
+    std::string theName;
+    std::map< std::string, std::string >::const_iterator matNamePos;
+    if ((matNamePos = _matNameMap.find(matName)) != _matNameMap.end()) {
+      theName = matNamePos->second;
+      theMat = createMaterial<T>( theName, matName);
+    } else {
+//then , try to find the material name directly
+      theMat = createMaterial<T>( matName, matName);
+// if we created a new material directly, add it to the list
+      if(theMat != 0)that()->declareMaterial(matName,matName);
+    }
+  }
+  if(theMat == 0){
+    ErrMsg( error ) << "MatDBInfo: Cannot find requested material " << matName
+                    << "." << endmsg;
+  }
+  return theMat;
+}
 
 #endif
