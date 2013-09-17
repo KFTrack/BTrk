db4e46161454706f92754647fb5c104206975de2
brownd <brownd1978@gmail.com>
2013-09-17 16:57:20 +0000

    Add displacement and error terms to BField correction

--- BTrk/KalmanTrack/src/KalBend.cc
+++ BTrk/KalmanTrack/src/KalBend.cc
@@ -10,12 +10,16 @@
 //  Authors: Dave Brown, 3/30/98
 //------------------------------------------------------------------------------
 #include "BaBar/BaBar.hh"
+#include "BField/BField.hh"
 #include "KalmanTrack/KalRep.hh"
 #include "KalmanTrack/KalBend.hh"
 #include "TrkBase/TrkDifPieceTraj.hh"
 #include "TrkBase/TrkParams.hh"
 using std::endl;
 using std::ostream;
+
+double KalBend::_efac(0.0);
+double KalBend::_berr(0.0);
 
 KalBend::KalBend(const BFieldIntegrator& integrator,
 		 const TrkDifPieceTraj* reftraj,BFieldIntRange const& range,
@@ -75,16 +79,40 @@ KalBend::updateCache(const TrkDifPieceTraj* reftraj) {
     _thetahat = _momhat.cross(_phihat).unit();
 // integrate the bfield over the range
     _delmom = _integrator.deltaMomentum(reftraj,_range);
-// find the dependence of the parameters on deflection in the 2 views
+// deflection
     HepMatrix t1deflect = localTrajectory()->derivDeflect(localLength(),theta1);
     HepMatrix t2deflect = localTrajectory()->derivDeflect(localLength(),theta2);
-// define the transport vector by applying these.  Note that no smearing
+// displacement
+    HepMatrix t1displace = localTrajectory()->derivDisplace(localLength(),theta1);
+    HepMatrix t2displace = localTrajectory()->derivDisplace(localLength(),theta2);
+ // define the transport vector by applying these.  Note that no smearing
 // is added.  The delta* functions take account of the charge-dependence
 // of the bend corrections.
+// angular deflection over this range
     double dtheta = deltaTheta();
     double dphi = deltaPhi();
     HepVector tvect(t1deflect*dtheta + t2deflect*dphi);
-    _transport = KalParams(-tvect,HepSymMatrix(tvect.num_row(),0));
+// displacement over this range
+    HepVector dvect(t1displace*dtheta*_range.range() + t2displace*dphi*_range.range());
+// compute the dimensionless error matrix.  The first term is from the uncertainty in the
+// field measurement and interpolation, the second from the tracjectory
+// notice I use the total momentum, not the transverse, as the field errors can be in any direction.  The 0.5 accounts
+// for assigning this error along two transverse directions.
+    double berr2 = pow(0.5*BField::mmTeslaToMeVc*_range.range()*_berr/_momentum,2);
+    double eerr2 = pow(0.5*_efac*_delmom.mag()/_momentum,2);
+    static const HepSymMatrix unit(1,1);
+    HepSymMatrix emat = unit*(berr2 + eerr2);
+    static const double invsqrt3(1.0/sqrt(3.0));
+// assume a 50% correlation between the deflection and displacement errors.  This models a random walk.    
+    const double cfac = 0.5*_range.range();
+    const double ufac = cfac*invsqrt3;
+    HepMatrix t1cor = t1deflect + cfac*t1displace;
+    HepMatrix t2cor = t2deflect + cfac*t2displace;
+    HepMatrix t1uncor = ufac*t1displace;
+    HepMatrix t2uncor = ufac*t2displace;
+// correlated terms add linearly (amplitude), uncorrelated add in quadrature
+    HepSymMatrix scatter = emat.similarity(t1cor) + emat.similarity(t2cor) + emat.similarity(t1uncor) + emat.similarity(t2uncor);
+    _transport = KalParams(-tvect-dvect,scatter);
   }
 // reset the site
   reset();
