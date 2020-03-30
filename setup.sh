1b3ae852826b4d80616e3d4254c9efc735b17f0a
Ray Culbertson <rlc@fnal.gov>
2020-03-30 15:19:59 -0500

    update root version for art 3.05.00

--- setup.sh
+++ setup.sh
@@ -38,7 +38,7 @@ fi
 # They must be maintained by hand.
 # It's OK if EXTRA_ROOT_QUALIFIERS is an empty string
 export PACKAGE_NAME=BTrk
-export PACKAGE_VERSION=v1_02_21
+export PACKAGE_VERSION=v1_02_22
 export COMPILER_CODE=e19
 export EXTRA_ROOT_QUALIFIERS=""
 
@@ -60,7 +60,7 @@ qualifiers=+${COMPILER_CODE}:+${DEBUG_LEVEL}
 extras=`echo ${EXTRA_ROOT_QUALIFIERS} | sed 's/:/:+/g'`
 
 setup -B clhep v2_4_1_2  -q${qualifiers}
-setup -B root  v6_18_04c -q${qualifiers}${extras}
+setup -B root  v6_18_04d -q${qualifiers}${extras}
 setup -B scons v3_1_1  -q p372
 
 # Only used inside scripts/install.sh, to get the flavor of the build platform.
