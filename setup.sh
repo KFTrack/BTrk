1caab1383f37d8ef1ce9cced9f4cdc153f9f5f6d
Rob Kutschke <kutschke@fnal.gov>
2021-03-17 12:12:40 -0500

    Update root, clhep, scons for art v3_08_00

--- setup.sh
+++ setup.sh
@@ -38,9 +38,9 @@ fi
 # They must be maintained by hand.
 # It's OK if EXTRA_ROOT_QUALIFIERS is an empty string
 export PACKAGE_NAME=BTrk
-export PACKAGE_VERSION=v1_02_30
+export PACKAGE_VERSION=v1_02_31
 export COMPILER_CODE=e20
-export EXTRA_ROOT_QUALIFIERS=":p383b"
+export EXTRA_ROOT_QUALIFIERS=":p392"
 
 # Done parsing and checking arguments
 
@@ -60,9 +60,9 @@ setup -B gcc v9_3_0
 qualifiers=+${COMPILER_CODE}:+${DEBUG_LEVEL}
 extras=`echo ${EXTRA_ROOT_QUALIFIERS} | sed 's/:/:+/g'`
 
-setup -B clhep v2_4_1_3a  -q${qualifiers}
-setup -B root  v6_22_06 -q${qualifiers}${extras}
-setup -B scons v3_1_2  -q p383b
+setup -B clhep v2_4_4_1  -q${qualifiers}
+setup -B root  v6_22_06b -q${qualifiers}${extras}
+setup -B scons v3_1_2a   -q p392
 
 # Only used inside scripts/install.sh, to get the flavor of the build platform.
 setup cetpkgsupport
