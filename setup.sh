0ab75edf02b214a11680a8d82dca9fa06420ab86
Rob Kutschke <kutschke@fnal.gov>
2021-07-08 14:03:54 -0500

    Update root version to v6_22_08c.

--- setup.sh
+++ setup.sh
@@ -38,7 +38,7 @@ fi
 # They must be maintained by hand.
 # It's OK if EXTRA_ROOT_QUALIFIERS is an empty string
 export PACKAGE_NAME=BTrk
-export PACKAGE_VERSION=v1_02_32
+export PACKAGE_VERSION=v1_02_33
 export COMPILER_CODE=e20
 export EXTRA_ROOT_QUALIFIERS=":p392"
 
@@ -61,7 +61,7 @@ qualifiers=+${COMPILER_CODE}:+${DEBUG_LEVEL}
 extras=`echo ${EXTRA_ROOT_QUALIFIERS} | sed 's/:/:+/g'`
 
 setup -B clhep v2_4_4_1  -q${qualifiers}
-setup -B root  v6_22_08  -q${qualifiers}${extras}
+setup -B root  v6_22_08c -q${qualifiers}${extras}
 setup -B scons v3_1_2a   -q p392
 
 # Only used inside scripts/install.sh, to get the flavor of the build platform.
