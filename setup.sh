f9258368d56713a1328cec69dbc4874cf32fa028
Rob Kutschke <kutschke@fnal.gov>
2023-02-28 17:22:12 -0600

    Update UPS products for art suite v3_13_01.

--- setup.sh
+++ setup.sh
@@ -38,9 +38,9 @@ fi
 # They must be maintained by hand.
 # It's OK if EXTRA_ROOT_QUALIFIERS is an empty string
 export PACKAGE_NAME=BTrk
-export PACKAGE_VERSION=v1_02_37
+export PACKAGE_VERSION=v1_02_38
 export COMPILER_CODE=e20
-export EXTRA_ROOT_QUALIFIERS=":p3913"
+export EXTRA_ROOT_QUALIFIERS=":p3915"
 
 # Done parsing and checking arguments
 
@@ -59,9 +59,9 @@ setup -B gcc v9_3_0
 qualifiers=+${COMPILER_CODE}:+${DEBUG_LEVEL}
 extras=`echo ${EXTRA_ROOT_QUALIFIERS} | sed 's/:/:+/g'`
 
-setup -B clhep v2_4_5_3a -q${qualifiers}
-setup -B root  v6_26_06  -q${qualifiers}${extras}
-setup -B scons v4_4_0    -q p3913
+setup -B clhep v2_4_6_4  -q${qualifiers}
+setup -B root  v6_28_00b -q${qualifiers}${extras}
+setup -B scons v4_4_0    -q p3915
 
 # Only used inside scripts/install.sh, to get the flavor of the build platform.
 setup cetpkgsupport
